ca5bda8f5d219d045c95fa284b9d3fd45bec0c28
Konstantin Chuykov <chuikoff.ru@gmail.com>
2018-11-26 16:42:57 +1200

    Add git url

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ router.get('/', (req, res) => {
 
 });
 
-router.post('/upload', upload.any(), async (req, res) => {
+router.post('/upload', upload.any(), (req, res) => {
 
     if (!req.headers['authorization'] || !getKeys().includes(req.headers['authorization']))
         return res.status(401).json({ error: 'Unauthorized' });
